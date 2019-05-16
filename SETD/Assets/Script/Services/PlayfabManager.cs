b0407ad68c9c313d4bb7b5598fe30a9493ebfe8b
Setthakritti <setthakritti@gmailcom>
2019-05-17 03:04:42 +0700

    + playfab sdk, photo sdk, zenject

---
+++ SETD/Assets/Script/Services/PlayfabManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayfabManager : MonoBehaviour
+{
+    #region  API
+
+    #region  Connection Resolver
+
+    // IEnumerator ConnectionResolver(Action api)
+    // {
+    //     api();
+    // }
+
+    #endregion
+
+    // async Task LoginTask(object task)
+    // {
+    //     await task();
+    // }
+    void PlayfabRegister(string username, string password)
+    {
+        var registerRequest = new RegisterPlayFabUserRequest
+        {
+            Username = username,
+            Password = password,
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, RegisterCallback, ErrorCallback);
+    }
+
+    void PlayfabLogin(string username, string password)
+    {
+        var loginRequest = new LoginWithPlayFabRequest
+        {
+            Username = username,
+            Password = password
+        };
+
+        PlayFabClientAPI.LoginWithPlayFab(loginRequest, LoginCallback, ErrorCallback);
+    }
+
+    #endregion
+
+    #region  Callback
+
+    void RegisterCallback(RegisterPlayFabUserResult result)
+    {
+
+    }
+
+    void LoginCallback(LoginResult result)
+    {
+
+    }
+
+    void ErrorCallback(PlayFabError error)
+    {
+
+    }
+
+    #endregion
+}
