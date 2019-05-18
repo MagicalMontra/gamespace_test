26dcc8bc37f32e167b81e686e652c2c414730a6d
Setthakritti <setthakritti@gmailcom>
2019-05-18 10:40:59 +0700

    * working on playfab character creation

--- SETD/Assets/Script/Services/PlayfabManager.cs
+++ SETD/Assets/Script/Services/PlayfabManager.cs
@@ -3,8 +3,11 @@ using System.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using PlayFab;
+using PlayFab.Json;
 using PlayFab.ClientModels;
 using UnityEngine;
+using UnityEngine.Serialization;
+using TMPro;
 
 public class PlayfabManager : MonoBehaviour
 {
@@ -23,12 +26,24 @@ public class PlayfabManager : MonoBehaviour
     // {
     //     await task();
     // }
+    public void OnLoginButtonClicked()
+    {
+        PlayfabLogin("test001", "111111");
+    }
+
+    public void OnRegisterButtonCLicked()
+    {
+        PlayfabRegister("test001", "111111");
+    }
+
     void PlayfabRegister(string username, string password)
     {
         var registerRequest = new RegisterPlayFabUserRequest
         {
+            TitleId = "18CA2",
             Username = username,
             Password = password,
+            RequireBothUsernameAndEmail = false,
         };
 
         PlayFabClientAPI.RegisterPlayFabUser(registerRequest, RegisterCallback, ErrorCallback);
@@ -51,17 +66,39 @@ public class PlayfabManager : MonoBehaviour
 
     void RegisterCallback(RegisterPlayFabUserResult result)
     {
-
+        Debug.Log("Created" + result.PlayFabId + " account completed");
     }
 
-    void LoginCallback(LoginResult result)
+    void LoginCallback(LoginResult loginResult)
     {
+        PlayFabClientAPI.ExecuteCloudScript(new ExecuteCloudScriptRequest()
+        {
+            FunctionName = "createCharacter",
+            FunctionParameter = new { characterName = "TestSubject" },
+            GeneratePlayStreamEvent = true
+        }, csResult =>
+        {
+            JsonObject jsonResult = (JsonObject)csResult.FunctionResult;
+            object message = null;
+            jsonResult.TryGetValue("CharacterId", out message);
+            var characterResult = JsonUtility.FromJson<GrantCharacterToUserResult>(message.ToString());
 
+            var newCharacterData = new Dictionary<string, string>();
+            newCharacterData.Add("Class", "Swordman");
+            newCharacterData.Add("Race", "Human");
+            var initCharacterRequest = new UpdateCharacterDataRequest()
+            {
+                CharacterId = characterResult.CharacterId,
+                Data = newCharacterData,
+                Permission = UserDataPermission.Public
+            };
+            PlayFabClientAPI.UpdateCharacterData(initCharacterRequest, null, ErrorCallback);
+        }, ErrorCallback);
     }
 
     void ErrorCallback(PlayFabError error)
     {
-
+        Debug.Log(error.GenerateErrorReport().ToString());
     }
 
     #endregion
